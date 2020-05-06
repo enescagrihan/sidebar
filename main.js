78fede8b0774af1e558daa5d2745216ab57896d0
kiremit <57963926+kiremit@users.noreply.github.com>
2020-05-06 19:00:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const sidebar = document.getElementById('sidebar');
+const openBtn = document.getElementById('open-btn');
+const closeBtn = document.getElementById('close-btn');
 
-document.getElementById('open-btn').addEventListener('click', () => {
+openBtn.addEventListener('click', () => {
     sidebar.style.width = '250px';
 });
 
-document.getElementById('close-btn').addEventListener('click', () => {
+closeBtn.addEventListener('click', () => {
     sidebar.style.width = '0px';
+})
+
+const modalBtn = document.getElementById('modal-btn');
+const popUp = document.getElementById('modal-bg');
+const modalClose = document.getElementById('modal-close');
+
+modalBtn.addEventListener('click', () => {
+    popUp.style.visibility = 'visible';
+})
+
+modalClose.addEventListener('click', () => {
+    popUp.style.visibility = 'hidden';
 })
